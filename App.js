044806006117289afd3c196acf02ed9aeb2df4ed
sokvebolkol <sokvebol.kol@gmail.com>
2021-03-31 23:01:35 +0700

    click button link to google map

--- App.js
+++ App.js
@@ -1,86 +1,57 @@
-// Example to Send Text SMS on Button Click in React Native
-// https://aboutreact.com/send-text-sms-in-react-native/
 
-// import React in our code
 import React, {useState} from 'react';
+import getDirections from 'react-native-google-maps-directions'
 
-// import all the components we are going to use
 import {
   SafeAreaView,
   StyleSheet,
   View,
   Text,
-  Linking,
-  TouchableOpacity,
-  TextInput,
+  Button
 } from 'react-native';
 
-// import SMS API
-import SendSMS from 'react-native-sms';
-
 const App = () => {
-  const [mobileNumber, setMobileNumber] = useState('9999999999');
-  const [bodySMS, setBodySMS] = useState(
-    'Please follow https://aboutreact.com',
-  );
 
-  const initiateSMS = () => {
-    // Linking.openURL(`sms:&addresses=null&body=My sms text`);
-    // Check for perfect 10 digit length
-    if (mobileNumber.length != 10) {
-      alert('Please insert correct contact number');
-      return;
-    }
+ const handleGetDirections = () => {
 
-    SendSMS.send(
-      {
-        // Message body
-        body: bodySMS,
-        // Recipients Number
-        recipients: [mobileNumber],
-        // An array of types that would trigger a "completed" response when using android
-        successTypes: ['sent', 'queued'],
+    const data = {
+       source: {
+        latitude: 11.544451,
+        longitude: 104.865837,
       },
-      (completed, cancelled, error) => {
-        if (completed) {
-          console.log('SMS Sent Completed');
-        } else if (cancelled) {
-          console.log('SMS Sent Cancelled');
-        } else if (error) {
-          console.log('Some error occured');
+      destination: {
+        latitude: 11.569035,
+        longitude: 104.895357,
+      },
+      params: [
+        {
+          key: "travelmode",
+          value: "driving"        // may be "walking", "bicycling" or "transit" as well
+        },
+        {
+          key: "dir_action",
+          value: "navigate"       // this instantly initializes navigation using the given travel mode
         }
-      },
-    );
-  };
+      ],
+      // waypoints: [
+      //   {
+      //     latitude: 11.544451,
+      //     longitude: 104.865837,
+      //   },
+      //   {
+      //     latitude: 11.569035,
+      //     longitude: 104.895357,
+      //   },
+      // ]
+    }
+ 
+    getDirections(data)
+  }
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <Text style={styles.titleText}>
-          Example to Send Text SMS on Button Click in React Native
-        </Text>
-        <Text style={styles.titleTextsmall}>Enter Mobile Number</Text>
-        <TextInput
-          value={mobileNumber}
-          onChangeText={(mobileNumber) => setMobileNumber(mobileNumber)}
-          placeholder={'Enter Conatct Number to Call'}
-          keyboardType="numeric"
-          style={styles.textInput}
-        />
-        <Text style={styles.titleTextsmall}>Enter SMS body</Text>
-        <TextInput
-          value={bodySMS}
-          onChangeText={(bodySMS) => setBodySMS(bodySMS)}
-          placeholder={'Enter Conatct Number to Call'}
-          keyboardType="numeric"
-          style={styles.textInput}
-        />
-        <TouchableOpacity
-          activeOpacity={0.7}
-          style={styles.buttonStyle}
-          onPress={initiateSMS}>
-          <Text style={styles.buttonTextStyle}>Send Message</Text>
-        </TouchableOpacity>
+    <View style={styles.container}>
+        <Button onPress={handleGetDirections} title="Get Directions" />
       </View>
     </SafeAreaView>
   );
@@ -94,31 +65,5 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 10,
     textAlign: 'center',
-  },
-  titleText: {
-    fontSize: 22,
-    textAlign: 'center',
-    fontWeight: 'bold',
-  },
-  titleTextsmall: {
-    marginVertical: 8,
-    fontSize: 16,
-  },
-  buttonStyle: {
-    justifyContent: 'center',
-    marginTop: 15,
-    padding: 10,
-    backgroundColor: '#8ad24e',
-  },
-  buttonTextStyle: {
-    color: '#fff',
-    textAlign: 'center',
-  },
-  textInput: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    width: '100%',
-    paddingHorizontal: 10,
   },
 });
